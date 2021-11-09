711e3275fd6633f3254df91bd1d2ef3b7180abe4
yuxuanwu17 <yuxuan.wu17@gmail.com>
2021-11-09 15:57:02 -0500

    1. update the feature_selection.py with newly cleaned data. 2. prepare to visualize the plots of feature importance in the model

--- feature_selection.py
+++ feature_selection.py
@@ -6,15 +6,25 @@ import time
 
 np.random.seed(33)
 df = pd.read_csv("data/Cleaned_dat_encoded.csv")
-X = df.iloc[:, :-1]
-Y = df.iloc[:, -1]
+Y = df["disposition"]
+X = df.drop("disposition", axis=1)
+
 start = time.time()
 selector = SelectKBest(mutual_info_classif, k=20).fit(X, Y)
 cols = selector.get_support(indices=True)
 end = time.time()
-print(cols)  # [ 2  4  5  6  8  9 10 12 22 23 26 45 47 49 53 59 60 63 64 68]
+print(cols)
 print(end - start)
-df = df.iloc[:, cols]
+df = X.iloc[:, cols]  # [  1   2   4   5   6   8   9  10  11  15 137 335 352 357 358 364 366 367 375 381]
+df["disposition"] = Y
 print(df.shape)
 print(df.columns)
-df.to_csv("data/featureSelectedAllData.csv")
+
+df.to_csv("data/featureSelectedAllDataWithY.csv", index=False)
+# cols = [1, 2, 4, 5, 6, 8, 9, 10, 11, 15, 137, 335, 352, 357, 358, 364, 366, 367, 375, 381]
+# X = X.iloc[:100, :]
+# Y = Y.iloc[:100]
+# print(Y.shape)
+# print(type(Y))
+# print(X.shape)
+# print(type(X))
