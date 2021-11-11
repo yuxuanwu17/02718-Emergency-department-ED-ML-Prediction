6d4e42da815b4337fdc9d05bd87df737fbb032d2
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2021-11-10 19:57:03 -0500

    Add code for data visualization

--- EDA/EDA_plots.R
+++ EDA/EDA_plots.R
@@ -7,16 +7,6 @@ library(ggcorrplot)
 library(scales)
 library(ggrepel)
 
-# only triage feature
-useonlytriage <- function(df) {
-  demographics <- 1:16
-  triage_vitals <- which(names(df) == 'triage_vital_hr'):which(names(df) == 'triage_vital_temp')
-  cc <- which(names(df) == 'cc_abdominalcramping'):which(names(df) == 'cc_wristpain')
-  onlytriage <- c(demographics, triage_vitals, cc)
-  
-  df[,onlytriage]
-}
-
 ########### correlation heatmap ##############################
 
 ## transform categorical and binary features to numeric feature
@@ -30,15 +20,24 @@ for (i in 1:ncol(clean_dat)) {
 cortest <- cor(df_trans)
 plot(cortest["disposition",] )
 mean(abs(cortest["disposition",]))
-ggcorrplot(cortest, hc.order = TRUE, outline.col = "white")
+
+## build correlation data frame
+diposition_corr <- data.frame(cor = cortest["disposition",],
+                              corabs = abs(cortest["disposition",]))
+diposition_corr_order <- diposition_corr[order(diposition_corr$corabs, 
+                                               decreasing = TRUE),]
+
+cor_test <- cor(df_trans[,which(colnames(df_trans) %in% 
+                                  rownames(diposition_corr_order)[1:30])])
+ggcorrplot(cor_test, hc.order = TRUE, outline.col = "white")
 
 
 ########### sex pie chart ##############################
 sexPie <- function(sexArray, titleText){
   sexDat <- data.frame(
     group = c("Male", "Female"),
-    value = c(sum(sexArray == "Male"), 
-              sum(sexArray == "Female"))
+    value = c(sum(sexArray == 2), 
+              sum(sexArray == 1))
   )
   
   bp <- ggplot(sexDat, aes(x="", y=value, fill=group))+
@@ -99,4 +98,38 @@ esiPie <- function(esiArray, titleText){
     ggtitle(titleText)
   p
 }
+
+########### age stack barplot ##############################
+## inspect on age
+summary(df_trans$age)
+age_start <- c(0,32,50,65)
+age_end <- c(32,50,65,108)
+age_interval_string <- c()
+for (i in 1:4){
+  current_age_string <- paste(age_start[i], 
+                              age_end[i],sep = "-")
+  age_interval_string <- c(age_interval_string, current_age_string)
+}
+
+age_df <- data.frame(label = rep(c("Admit", "Discharge"), each = 4),
+                     age = rep(age_interval_string, 2),
+                     age_start = rep(age_start, 2),
+                     age_end = rep(age_end, 2),
+                     value = 0)
+
+## fill in the value field of the age data frame
+for (i in 1:nrow(age_df)){
+  age_df[i, "value"] <- nrow(clean_dat[which(clean_dat$disposition == age_df[i,1] &
+                                               clean_dat$age >= age_df[i,3] & 
+                                               clean_dat$age < age_df[i,4]),])
+  if (i == 4 | i == 8) {
+    age_df[i, "value"] <- nrow(clean_dat[which(clean_dat$disposition == age_df[i,1] &
+                                                 clean_dat$age >= age_df[i,3] & 
+                                                 clean_dat$age <= age_df[i,4]),])
+  }
+}
+
+# Stacked barplot
+ggplot(age_df[,c(1,2,5)], aes(fill=label, y=value, x=age)) + 
+  geom_bar(position="fill", stat="identity")
 
